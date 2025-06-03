898102e402c4d026206f7ac1207ce9abb8cdbdc1
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-06-03 16:05:58 -0400

    fimshed

--- basic-queries/Ex 2 pg 51 - 52 WB 7 pt 2.sql
+++
@@ -1,6 +0,0 @@
--- 10. Examine the Products table. How does it identify the type (category) of
--- each item sold? Write a query to list all of the seafood items we carry.
-
-SELECT ProductName, CategoryID
-FROM products
-WHERE CategoryID = 'Seafood';
